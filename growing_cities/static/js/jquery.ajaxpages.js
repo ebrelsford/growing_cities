c0f442107e11ece708018887a54a32ab6bfc94b9
Eric Brelsford <ebrelsford@gmail.com>
2013-03-05 01:42:12 -0500

    keep animation

--- growing_cities/static/js/jquery.ajaxpages.js
+++ growing_cities/static/js/jquery.ajaxpages.js
@@ -123,7 +123,7 @@
 			// Start Fade Out
 			// Animating to opacity to 0 still keeps the element's height intact
 			// Which prevents that annoying pop bang issue when loading in new content
-			//$content.animate({ opacity: 0 },800);
+			$content.animate({ opacity: 0 },800);
 
             $window.trigger(startedEventName);
 
