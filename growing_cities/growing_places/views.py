4cf08f717ae5e360473cd5b5688e69feb406b076
Eric Brelsford <ebrelsford@gmail.com>
2013-03-06 02:03:10 -0500

    map plugin

--- growing_cities/growing_places/views.py
+++ growing_cities/growing_places/views.py
@@ -1,14 +1,33 @@
 from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView
 
 from fiber.views import FiberPageMixin
-from inplace.views import PlacesListView
 
 from .models import GrowingPlace
 
 
-class GrowingPlacesMapView(FiberPageMixin, PlacesListView):
-    queryset = GrowingPlace.objects.filter(centroid__isnull=False)
+class GrowingPlacesMapView(FiberPageMixin, TemplateView):
     template_name = 'growing_places/map.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(GrowingPlacesMapView, self).get_context_data(**kwargs)
+        context.update({
+            'cities': self._get_cities(),
+            'places_url': reverse('inplace:growing_places_growingplace_geojson'),
+        })
+        print context
+        return context
 
     def get_fiber_page_url(self):
         return reverse('growing_places_map')
+
+    def _get_cities(self):
+        places = GrowingPlace.objects.filter(
+            centroid__isnull=False,
+            city__isnull=False,
+            state_province__isnull=False,
+        )\
+        .distinct('city', 'state_province')\
+        .order_by('state_province', 'city')
+        cities_states = places.values_list('city', 'state_province')
+        return [', '.join((city, state)) for (city, state) in cities_states]
---
+++ growing_cities/static/js/jquery.map.js
@@ -0,0 +1,127 @@
+(function (window, $, undefined) {
+    "use strict";
+
+    $.placemap = function(options, element) {
+        this.element = $(element);
+        if (!this._create(options)) {
+            this.failed = true;
+        }
+    };
+
+    $.placemap.defaults = {
+        apiKey: null,
+        styleId: null,
+    };
+
+    $.placemap.prototype = {
+
+        _create: function(options) {
+            // Add custom options to defaults
+            var opts = $.extend(true, {}, $.placemap.defaults, options);
+            this.options = opts;
+            var $window = $(window);
+            var instance = this;
+
+            instance._initializeMap();
+
+            return true;
+        },
+
+        _initializeMap: function() {
+            var instance = this;
+            instance.map = L.map(instance.element[0].id);
+
+            var cloudmade = new L.TileLayer(
+                'http://{s}.tile.cloudmade.com/{apiKey}/{styleId}/256/{z}/{x}/{y}.png', 
+                {
+                    apiKey: instance.options.apiKey,
+                    attribution: 'Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+                    maxZoom: 18,
+                    styleId: instance.options.styleId,
+                }
+            );
+
+            instance.map.addLayer(cloudmade);
+
+            var latlngs = [];
+            var feature_layer = new L.GeoJSON(null, {
+
+                onEachFeature: function(data, layer) {
+                    layer.on('click', function(e) {
+                        $.get(layer.feature.properties.popup_url, function(data) {
+                            layer.bindPopup(data).openPopup();
+                        });
+                    });
+                },
+
+                pointToLayer: function(data, latlng) {
+                    latlngs.push(latlng);
+                    return new L.marker(latlng);
+                },
+
+            });
+
+            $.get(instance.options.placesUrl, function(collection) {
+                feature_layer.addData(collection);
+                instance.map.addLayer(feature_layer);
+                instance.map.fitBounds(new L.LatLngBounds(latlngs));
+            });
+
+        },
+    };
+
+    $.fn.placemap = function(options) {
+        var thisCall = typeof options;
+
+        switch (thisCall) {
+
+            // method 
+            case 'string':
+                var args = Array.prototype.slice.call(arguments, 1);
+
+                this.each(function() {
+
+                    var instance = $.data(this[0], 'placemap');
+
+                    if (!instance) {
+                        // not setup yet
+                        return $.error('Method ' + options + 
+                            ' cannot be called until placemap is setup');
+                    }
+
+                    if (!$.isFunction(instance[options]) ||
+                            options.charAt(0) === "_") {
+                        return $.error('No such method ' + options + ' for placemap');
+                    }
+
+                    // no errors!
+                    return instance[options].apply(instance, args);
+                });
+
+                break;
+
+            // creation 
+            case 'object':
+
+                this.each(function () {
+                    var instance = $.data(this, 'placemap'); 
+                    if (instance) {
+                        // update options of current instance
+                        instance.update(options);
+                    } else {
+                        // initialize new instance
+                        instance = new $.placemap(options, this);
+
+                        // don't attach if instantiation failed
+                        if (!instance.failed) {
+                            $.data(this, 'placemap', instance);
+                        }
+                    }
+                });
+
+                break;
+        }
+        return this;
+    };
+
+})(window, jQuery);
