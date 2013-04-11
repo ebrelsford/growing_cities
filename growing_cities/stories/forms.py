7ffe5151588221781e4e07f13846015939401700
Eric Brelsford <ebrelsford@gmail.com>
2013-04-10 22:36:57 -0400

    placeholders for story form

--- growing_cities/stories/forms.py
+++ growing_cities/stories/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from moderation.forms import BaseModeratedObjectForm
 
@@ -6,6 +7,28 @@ from .models import Story
 
 
 class StoryForm(BaseModeratedObjectForm):
+    title = forms.CharField(
+        label=_(u'Title'),
+        max_length=200,
+        widget=forms.TextInput(attrs={ 'placeholder': _(u'title'), }),
+    )
+
+    video_url = forms.CharField(
+        label=_(u'Video URL'),
+        max_length=200,
+        required=False,
+        widget=forms.TextInput(attrs={ 'placeholder': _(u'video URL'), }),
+    )
+
+    text = forms.CharField(
+        label=_(u'Text'),
+        max_length=200,
+        required=False,
+        widget=forms.Textarea(attrs={
+            'placeholder': _(u'the text of your story'),
+        }),
+    )
+
     featured = forms.BooleanField(
         initial=False,
         required=False,
