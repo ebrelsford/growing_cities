58f8da4aa22c5d20f134d3d27082d09edf83e4c9
Eric Brelsford <ebrelsford@gmail.com>
2013-04-29 16:41:36 -0400

    shorten placeholder text

--- growing_cities/growing_places/forms.py
+++ growing_cities/growing_places/forms.py
@@ -18,7 +18,7 @@ class GrowingPlaceForm(BaseModeratedObjectForm):
         label=_('How can people get involved?'),
         queryset=Activity.objects.all(),
         required=False,
-        widget=forms.SelectMultiple(attrs={'data-placeholder': 'Pick activities'}),
+        widget=forms.SelectMultiple(attrs={'data-placeholder': 'Pick'}),
     )
     name = forms.CharField(
         label="What's your project name?",
