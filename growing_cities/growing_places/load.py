7995a92e8409b108c32801d29449021bcd3beebc
Eric Brelsford <ebrelsford@gmail.com>
2013-04-30 01:01:34 -0400

    load places with activities

--- growing_cities/growing_places/load.py
+++ growing_cities/growing_places/load.py
@@ -5,22 +5,49 @@ import traceback
 from django.conf import settings
 from django.contrib.gis.geos import Point
 
+from moderation import moderation
+
 from inplace.geocode.mapquest import geocode
-from .models import GrowingPlace
+from .models import Activity, GrowingPlace
+
+
+activities = {
+    'Dig in Dirt': 1,
+    'Change the Law': 2,
+    'Learn skills': 4,
+    'Get your kids involved': 3,
+    'Help your community thrive': 5,
+    'Buy or trade good food': 6,
+    'Cook healthy food': 7,
+    'Create new growing spaces': 8,
+}
+
+
+def _get_activities(place):
+    return [Activity.objects.get(pk=activities[a]) for a in activities
+            if place[a] is not '']
+
+
+def _strip_string(s):
+    """Strip a string and get rid of any unicode"""
+    return unicode(s, 'utf-8', 'ignore').strip().encode('ascii', 'ignore')
 
 
 def load_growing_places(file_name='growing_places.csv'):
     reader = csv.DictReader(open(settings.DATA_ROOT + '/' + file_name))
+
+    # Stop moderating GrowingPlace objects for now
+    moderation.unregister(GrowingPlace)
 
     for place in reader:
         # skip empty rows
         if not place['Organization']:
             continue
 
-        address = place['Address'].strip()
-        city = place['City'].strip()
-        state = place['State'].strip()
-        zipcode = place['ZIP'].strip()
+        address = _strip_string(place['Address'])
+        city = _strip_string(place['City'])
+        state = _strip_string(place['State'])
+        zipcode = _strip_string(place['ZIP'])
         # watch out for weird stuff in the zipcode field
         if not re.match('^[\d-]+$', zipcode):
             zipcode = None
@@ -38,7 +65,7 @@ def load_growing_places(file_name='growing_places.csv'):
                 state_province=state,
 
                 defaults={
-                    'address_line1': place['Address'].strip(),
+                    'address_line1': address,
                     'centroid': centroid,
                     'country': 'USA',
                     'mission': place['Mission'].strip(),
@@ -47,8 +74,13 @@ def load_growing_places(file_name='growing_places.csv'):
                     'url': place['Website'].strip(),
                 },
             )
+            saved_place.activities = _get_activities(place)
+            saved_place.save()
             print 'added', saved_place.name
         except Exception:
             print 'Exception while adding "%s"' % place['Organization']
             traceback.print_exc()
             continue
+
+    # Start moderating GrowingPlace objects again
+    moderation.register(GrowingPlace)
