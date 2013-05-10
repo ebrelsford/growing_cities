2f9ac2a781e71150304fc2242d8c350e4b37ec7d
Eric Brelsford <ebrelsford@gmail.com>
2013-05-10 13:24:57 -0400

    Don't inform moderators if object has been approved

--- growing_cities/growing_cities/moderators.py
+++ growing_cities/growing_cities/moderators.py
@@ -1,16 +1,18 @@
 from django.contrib.sites.models import Site
 
+from moderation.models import MODERATION_STATUS_APPROVED
 from moderation.moderator import GenericModerator
 
 
 class SiteModerator(GenericModerator):
+    auto_approve_for_staff = True
     auto_reject_for_anonymous = False
     notify_moderator = True
     notify_user = False
 
-    def inform_moderator(self, content_object, extra_context=None):
-        extra_context = {
-            'site': Site.objects.get_current(),
-        }
-        super(SiteModerator, self).inform_moderator(content_object,
-                                                      extra_context)
+    def inform_moderator(self, obj, extra_context={}):
+        # Don't inform moderators if object has been approved
+        if obj.moderated_object.moderation_status == MODERATION_STATUS_APPROVED:
+            return
+        extra_context['site'] = Site.objects.get_current()
+        super(SiteModerator, self).inform_moderator(obj, extra_context)
