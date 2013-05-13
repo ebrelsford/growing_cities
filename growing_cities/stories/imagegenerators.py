02986601ad332e2a98b7e7e17b3cf4f23fb43d88
Eric Brelsford <ebrelsford@gmail.com>
2013-05-13 00:09:37 -0400

    story list image dimensions

--- growing_cities/stories/imagegenerators.py
+++ growing_cities/stories/imagegenerators.py
@@ -1,9 +1,19 @@
 from imagekit import ImageSpec, register
-from imagekit.processors import ResizeToFit
+from imagekit.processors import ResizeToFit, SmartResize
+
 
 class DetailThumbnail(ImageSpec):
     processors = [ResizeToFit(width=600, height=600, upscale=False)]
     format = 'PNG'
     options = {'quality': 90}
 
+
+class ListThumbnail(ImageSpec):
+    # Weird dimensions to match videos in the story list view
+    processors = [SmartResize(width=236, height=145, upscale=False)]
+    format = 'PNG'
+    options = {'quality': 90}
+
+
 register.generator('stories:detailthumbnail', DetailThumbnail)
+register.generator('stories:listthumbnail', ListThumbnail)
